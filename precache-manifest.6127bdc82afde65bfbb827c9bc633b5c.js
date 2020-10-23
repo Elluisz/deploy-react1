39f6e2377a6d80832346bf431a4fa8d812fa1b73
Elluisz <juanluisazul@gmail.com>
2020-10-23 00:04:00 -0600

    Updates

---
+++ precache-manifest.6127bdc82afde65bfbb827c9bc633b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106bb184644bcc8bf4d492a39e6b3e39",
+    "url": "/deploy-react1/index.html"
+  },
+  {
+    "revision": "3f5989e4d542db630078",
+    "url": "/deploy-react1/static/css/main.b249a9b0.chunk.css"
+  },
+  {
+    "revision": "51c4a9e8771704b81e00",
+    "url": "/deploy-react1/static/js/2.c5048036.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/deploy-react1/static/js/2.c5048036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5989e4d542db630078",
+    "url": "/deploy-react1/static/js/main.aa4ca7da.chunk.js"
+  },
+  {
+    "revision": "b94cd8088e20870a6036",
+    "url": "/deploy-react1/static/js/runtime-main.c96d0234.js"
+  }
+]);
